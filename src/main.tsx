dbbaeec9dd546e6c49104b7d5baa1fba1de3f90a
rodrigoaraujo7 <dev.rodrigoaraujo@gmail.com>
2025-04-24 16:09:19 -0300

    feat: basic route structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
 import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />}  />
+
+        <Route path="sign-in" element={<h1>Sign in</h1>}  />
+        
+        <Route path="*" element={<h1>page not found</h1>}  />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );

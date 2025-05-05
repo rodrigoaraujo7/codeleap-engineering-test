2793df3cedea864d73560e42319682586c33eeb7
rodrigoaraujo7 <dev.rodrigoaraujo@gmail.com>
2025-05-04 21:23:31 -0300

    feat: home route and main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import "./global.css";
+
 import { BrowserRouter, Route, Routes } from "react-router";
+
 import { SignIn } from "./pages/SignIn";
+import { Home } from "./pages/Home";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<h1 className="text-5xl">Home page</h1>}  />
+        <Route index element={<Home />} />
 
-        <Route path="sign-in" element={<SignIn />}  />
-        
-        <Route path="*" element={<h1>page not found</h1>}  />
+        <Route path="sign-in" element={<SignIn />} />
+
+        <Route path="*" element={<h1>page not found</h1>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+export const Home = () => {
+  return (
+    <main className="flex justify-center">
+      <div className="w-3xl h-fit bg-white">
+        <header className="w-full h-20 px-9 flex items-center bg-light-blue border-b-1 border-gray-600">
+          <h1 className="font-bold text-2xl text-white">CodeLeap Network</h1>
+        </header>
+      </div>
+    </main>
+  )
+}

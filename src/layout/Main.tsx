6ff242de7180193ac0fe18ac36755095a75b767a
rodrigoaraujo7 <dev.rodrigoaraujo@gmail.com>
2025-05-07 17:24:43 -0300

    refactor: work with grid in main layout to better responsive

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -6,8 +6,8 @@ type MainProps = {
 
 export const Main = ({ ...props }: MainProps) => {
   return (
-    <main className="flex justify-center">
-      <div className="w-3xl h-dvh bg-white">
+    <main className="grid grid-cols-1 md:grid-cols-(--custom-grid-cols)">
+      <div className=" bg-white col-start-1 col-end-2 md:col-start-2 md:col-end-3">
         <header className="w-full h-20 px-9 flex items-center bg-light-blue border-b-1 border-gray-600">
           <h1 className="font-bold text-2xl text-white">CodeLeap Network</h1>
         </header>
